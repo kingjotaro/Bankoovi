00c1dc6e91c857b6d09a98875e2571847f76c9cd
rafael <rafaelleet@gmail.com>
2024-05-29 23:08:19 -0300

    ajusted few ts errors

--- apps/server/src/database/conectionDatabase.ts
+++ apps/server/src/database/conectionDatabase.ts
@@ -1,11 +1,15 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export default async function connectToMongoDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const mongoUri = process.env.MONGO_URI;
+    if (!mongoUri) {
+      throw new Error("MONGO_URI is not defined in the .env file");
+    }
+    await mongoose.connect(mongoUri);
     console.log("Successfully connected to MongoDB");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
