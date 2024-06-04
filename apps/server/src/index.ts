3f3beac7115c7ce2927a823ad79a76fcaddc37da
rafael <rafaelleet@gmail.com>
2024-06-04 06:45:19 -0300

    update imports on index

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,32 +3,31 @@ import path from 'path';
 import { ApolloServer } from 'apollo-server';
 import { buildSchema } from 'type-graphql';
 import connectToMongoDB from './database/conectionDatabase';
-import { accountResolver } from './resolvers/accountResolver'; 
+import { AccountResolver } from './resolvers/accountResolver'; 
 import { UserResolver } from "./resolvers/userResolver";
+import Transaction from "./database/schemas/transactionModel";
 
 
 async function app() {
-  // Conecte-se ao MongoDB
+
   await connectToMongoDB();
 
-  // Crie o schema GraphQL
+
   const schema = await buildSchema({
-    resolvers: [accountResolver, UserResolver],
+    resolvers: [AccountResolver, UserResolver, Transaction],
     emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
     validate: false,
   });
 
-  // Inicialize o servidor Apollo
+
   const server = new ApolloServer({
     schema,
   });
 
-  // Inicie o servidor na porta especificada ou na porta padrão (4000)
   const { url } = await server.listen();
   console.log(`Server running at ${url}`);
 
   
 }
 
-// Chame a função app para iniciar o servidor
 app();
